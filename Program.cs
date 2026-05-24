70b97f9c9691fc36f275c04590c02e618b358d98
joao vendrameto <joaovendrameto5mh@gmail.com>
2026-05-24 02:26:08 -0300

    refactor: enhance Swagger configuration and update docker-compose for environment variables

--- Program.cs
+++ Program.cs
@@ -1,33 +1,14 @@
-using celticsTech.Data;
-using celticsTech.Middlewares;
-using celticsTech.Repositories;
-using celticsTech.Services;
-using Microsoft.EntityFrameworkCore;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Configuração simples para Banco em Memória
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseInMemoryDatabase("CelticsDb"));
-
-builder.Services.AddControllers();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IPetRepository, PetRepository>();
-builder.Services.AddScoped<IVeterinarianRepository, VeterinarianRepository>();
-builder.Services.AddScoped<IConsultationRepository, ConsultationRepository>();
-builder.Services.AddScoped<UserService>();
-builder.Services.AddScoped<PetService>();
-builder.Services.AddScoped<VeterinarianService>();
-builder.Services.AddScoped<ConsultationService>();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 app.UseMiddleware<ErrorHandlingMiddleware>();
-app.UseSwagger();
-app.UseSwaggerUI(options => { options.RoutePrefix = string.Empty; });
+
+// Habilita o Swagger globalmente
+app.UseSwagger(); 
+app.UseSwaggerUI(options => 
+{ 
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "celticsTech API v1");
+    options.RoutePrefix = string.Empty; // Isso faz o Swagger abrir direto no http://ip:8080/
+});
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
